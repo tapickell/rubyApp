65346eef35304871adc87f36e2633e3c9c1ba084
Todd Pickell <toddpickell@me.com>
2012-10-13 10:39:59 -0500

    Initial Rails Setup for app

---
+++ myRubyz/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyRubyz::Application.config.secret_token = 'ca4ff6130b7a4b2d3e086f66347ffc8e4fa188b4c913f6d8c9b9568d26e2f03f62cd47dd58e97d7a9961c969b7f5a9192c5a3d60adf31861ddb1725e1180e35d'
